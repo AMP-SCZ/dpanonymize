b15af3779393c2aea691a9b991e7be9879f59aa5
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-06-25 21:32:54 -0400

    init: lockness modules initialized

---
+++ lockness/__version__.py
@@ -0,0 +1,7 @@
+__title__ = 'lockness'
+__description__ = 'PII remover'
+__url__ = 'https://github.com/PREDICT-DPACC/lockness'
+__version__ = '0.1'
+__author__ = 'DPACC'
+__author_email__ = 'kevincho@bwh.harvard.edu'
+
---
+++ lockness/video/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import shutil
+from typing import List
+
+def remove_pii(data_in: List[str, Path], data_out: List[str, Path]):
+    '''Remove PII from video data - place holder'''
+    shutil.copy(data_in, data_out)
+
