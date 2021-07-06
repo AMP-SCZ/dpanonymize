77f21cd74240636f41c1619087350a8fd9c06ca4
Tashrif Billah <tashrifbillah@gmail.com>
2021-07-06 15:34:41 -0400

    v is not needed here before version string

--- dpanonymize/__version__.py
+++ dpanonymize/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'dpanonymize'
 __description__ = 'PII remover'
 __url__ = 'https://github.com/AMP=SCZ/dpanonymize'
-__version__ = 'v0.1'
+__version__ = '0.1'
 __author__ = 'DPACC'
 __author_email__ = 'kevincho@bwh.harvard.edu'
